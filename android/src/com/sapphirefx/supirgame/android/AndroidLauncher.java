71d09629d16c08d77446d2240c306bb98982dc2a
SapphireFX <sapphireevgn@gmail.com>
2015-09-02 16:07:01 +0500

    Initial commit

---
+++ android/src/com/sapphirefx/supirgame/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.sapphirefx.supirgame.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sapphirefx.supirgame.SupirGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SupirGame(), config);
+	}
+}
---
+++ desktop/src/com/sapphirefx/supirgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sapphirefx.supirgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sapphirefx.supirgame.SupirGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SupirGame(), config);
+	}
+}
